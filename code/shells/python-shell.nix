27644e77c90471f8b07b81e73f06c610bc5ef7c1
Jason Odoom <jason.odoom@cisa.dhs.gov>
2023-06-12 20:07:27 -0400

    Add hash and additonal dependencies

--- code/shells/python-shell.nix
+++ code/shells/python-shell.nix
@@ -1,14 +1,26 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/eabc38219184cc3e04a974fe31857d8e0eac098d.tar.gz") {} }:
+{ pkgs ? import (fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz";
+                                sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+}) {} }:
 
 pkgs.mkShell {
   packages = [
-    (pkgs.python3.withPackages (ps: [
+    (pkgs.python311.withPackages (ps: [
       ps.pip
       ps.ipython
       ps.semver
       ps.virtualenv
       ps.setuptools
       ps.wheel
+      ps.docopt
+      ps.urllib3
+      ps.mypy
+      ps.semver
+      ps.pyyaml
+      ps.pluggy
+      ps.packaging
+      ps.nodeenv
+      ps.more-itertools
+      ps.iniconfig
     ]))
 
     pkgs.curl
